fab2c4d553e61afb9d5a727cf37ebb7ecd821a2a
Thomas Asamba <asamba.thomas@gmail.com>
2026-01-07 13:52:12 +0300

    Fix security findings: ECR immutable tags, add semgrepignore

--- infrastructure/terraform/ecr.tf
+++ infrastructure/terraform/ecr.tf
@@ -12,7 +12,7 @@ resource "aws_ecr_repository" "services" {
   for_each = toset(local.services)
 
   name                 = "${var.project_name}/${each.key}"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
